4035c7e39d0d82c58624cde0dcd9477daaeb3094
d8888 <smallt@gmail.com>
2021-04-03 19:00:43 +0800

    update extraction tool and dictionary

--- tools/pdfextract.py
+++ tools/pdfextract.py
@@ -6,13 +6,22 @@ import json
 import argparse
 import itertools
 
+maxPhraseWord = 4
+minWordLen = 3
+
+def checkKey(key):
+    global minWordLen
+    for elem in key:
+        if len(elem)<minWordLen:
+            return False
+    return True
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-f','--file', nargs='+', help='input files', required=True)
 args = parser.parse_args()
 files = args.file
 print(files)
 
-wordset = []
 
 wordgroup = {}
 
@@ -20,24 +29,42 @@ for pdffile in files:
     with fitz.open(pdffile) as doc:
         for i, page in enumerate(doc):
             print("parsing page "+str(i+1)+" in "+str(pdffile))
-            text = page.getText()
-            rst = list(set([x.group().lower() for x in re.finditer( r'([a-zA-Z]{3,})', text)]))
-            wordset = itertools.chain(wordset, rst)
+            
 
-            # 詞組
-            rst = list(set([x.group().lower() for x in re.finditer( r'([a-zA-Z]{2,} +){2,4}', text)]))
-            for elem in rst:
-                if not elem in wordgroup:
-                    wordgroup[elem]=1
-                wordgroup[elem]+=1
+            text = page.getText()
+            rst = [x.group().lower() for x in re.finditer( r'([a-zA-Z]{1,})', text)]
+
+            nword = len(rst)
+            for k in range(0, nword):
+                for j in range(1, maxPhraseWord):
+                    if k+j>nword:
+                        continue
+
+                    slce = rst[k:k+j]
+                    if not checkKey(slce):
+                        continue
+                    key = " ".join(slce).lower()
+                    
+                    if key not in wordgroup:
+                        wordgroup[key]=0
+                    wordgroup[key]+=1
+
             
 print("preparing data for final dumping")            
-wordset = list(set(wordset))
 
+wordset = []
 for elem in wordgroup:
-    if wordgroup[elem]>5:
+    nwords = len(elem.split())
+    if nwords==1:
         wordset.append(elem.strip())
+    elif nwords==2:
+        if wordgroup[elem]>5:
+            wordset.append(elem.strip())
+    else:
+        if wordgroup[elem]>2:
+            wordset.append(elem.strip())
 
+wordset = list(set(wordset))    
 wordset.sort()
 
 print("dumping file....")
